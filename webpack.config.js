a8395add2015a901d9f45c86a3f19d0118dfaf43
Nemo <viole_day@live.com>
2015-12-30 15:40:25 +0800

    add:修复pkg,添加bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/app.js'),
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: "bundle.js"
